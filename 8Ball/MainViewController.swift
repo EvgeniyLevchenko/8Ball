a7fdb028c4c65478bb01869c86cda502bc3b00a4
EvgeniyLevchenko <evgeniy.levchnko@gmail.com>
2022-01-24 15:20:05 +0200

    Initial Commit

--- 8Ball/MainViewController.swift
+++ 8Ball/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  8Ball
 //
 //  Created by QwertY on 24.01.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
